d5ddf1ed0ebba57c06e9cab61d8595fa866a994a
oussama.touati <oussama.touati@enicar.ucar.tn>
2024-08-24 11:18:49 +0100

    CI/CD pipeline debuging 5 test final

--- gpao-tex-core/partie-prenante/src/main/java/com/gpro/consulting/partieprenante/controller/PartieInteresseeController.java
+++ gpao-tex-core/partie-prenante/src/main/java/com/gpro/consulting/partieprenante/controller/PartieInteresseeController.java
@@ -20,7 +20,7 @@ public class PartieInteresseeController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello world Test CI/CD 12";
+        return "Hello world Test CI/CD last time";
     }
     @PostMapping("/creerPi")
     public ResponseEntity<String> createPi(@RequestBody @Valid PartieInteresseValue request) {
