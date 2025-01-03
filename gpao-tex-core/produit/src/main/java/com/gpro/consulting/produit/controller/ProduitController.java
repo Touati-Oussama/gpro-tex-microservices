d056858838721ac9f9aaad166b033ee4d46d34ac
oussama touati <oussama.touati.178@gmail.com>
2025-01-03 11:53:13 +0100

    Added SonarQube v4

--- gpao-tex-core/produit/src/main/java/com/gpro/consulting/produit/controller/ProduitController.java
+++ gpao-tex-core/produit/src/main/java/com/gpro/consulting/produit/controller/ProduitController.java
@@ -21,7 +21,7 @@ public class ProduitController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello, world From Test CI/CD  SonarQube final test !";
+        return "Hello, world From Test CI/CD  SonarQube f inal test !";
     }
     @PostMapping("/createProduit")
     public ResponseEntity<String> createProduit(@RequestBody @Valid ProduitValue request) {
