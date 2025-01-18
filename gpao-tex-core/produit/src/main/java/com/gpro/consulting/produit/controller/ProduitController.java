31728d1179f2a1fb8b66708b60cbd5a8cac2ba8b
oussama touati <oussama.touati.178@gmail.com>
2025-01-18 15:40:37 +0100

    DevSecOps Test

--- gpao-tex-core/produit/src/main/java/com/gpro/consulting/produit/controller/ProduitController.java
+++ gpao-tex-core/produit/src/main/java/com/gpro/consulting/produit/controller/ProduitController.java
@@ -21,7 +21,7 @@ public class ProduitController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello, world From Test CI/CD  Test For DevSecOps Integrated fffffff !";
+        return "Hello, world From Test CI/CD  Test For DevSecOps Integrated ok ok ok  !";
     }
     @PostMapping("/createProduit")
     public ResponseEntity<String> createProduit(@RequestBody @Valid ProduitValue request) {
